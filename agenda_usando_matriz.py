342f2d42b33ae5b52fb56964f635a156c97573e9
Lucas Teixeira Ronchi <lucas0headshot@gmail.com>
2022-11-11 19:52:21 -0300

    Adicioando agenda_usando_matriz.py
    Algoritmo em Python que utiliza uma matriz para criar uma agenda

---
+++ agenda_usando_matriz.py
@@ -0,0 +1,17 @@
+#Teste de matriz
+
+i, j = 0 #Variáveis
+agenda = [] #Matriz
+
+for i in range(4): #For para preencher a matriz
+  agenda.append[i][0](input("Digite o dia(Da semana): "))
+  agenda.append[i][1](input("Digite o dia(Número): "))
+  agenda.append[i][2](input("Digite o hora: "))
+  agenda.append[i][3](input("Digite a tarefa: "))
+  printf("")  
+
+printf("")
+
+for j in range(4): #For para percorrer a matriz
+  printf("" + agenda[j][0] + " - Dia: " + agenda[j][1])
+  printf("Hora: " + agenda[j][2] + " - Tarefa: " + agenda[j][3])
